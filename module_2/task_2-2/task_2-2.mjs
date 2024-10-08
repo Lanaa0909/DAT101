f763ebad9f17c0e18e182f8c06551ba92af8e41f
Lanaa0909 <LanaSaid0909@gmail.com>
2024-10-07 19:05:43 -0700

    finished task

--- module_2/task_2-2/task_2-2.mjs
+++ module_2/task_2-2/task_2-2.mjs
@@ -84,8 +84,8 @@ The answer must be in whole "Kroner" and whole "dollars".
 
 //Given Data
  const usdAmount = 54;
- exchangeRateNOKtoUSD = 8.6 / 76; // 1NOK in USD
- exchangeRateUSDtoNOK = 76 / 8.6; //1 USD in NOK
+ const exchangeRateNOKtoUSD = 8.6 / 76; // 1NOK in USD
+ const exchangeRateUSDtoNOK = 76 / 8.6; //1 USD in NOK
 
 // Convert USD to NOK
  const nokAmount = Math.floor(usdAmount * exchangeRateUSDtoNOK);
@@ -94,34 +94,108 @@ The answer must be in whole "Kroner" and whole "dollars".
  const usdConvertedBack = Math.floor(nokAmount * exchangeRateNOKtoUSD);
  
  printOut(" USD to NOK = " + nokAmount + " NOK" );
- printOut("NOK to USD = " + usdConvertedBack + " USD");
+ printOut(" NOK to USD = " + usdConvertedBack + " USD");
  
 
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+/* Create a variable that contains the following text:
+"There is much between heaven and earth that we do not understand."
+● Print the number of characters in the text.
+● Print the character at position number 19.
+● Print the characters starting at position number 35 and 8 characters forward.
+● Print the index at which "earth" starts in the text.
+*/
+
+const text = "There is much between heaven and earth that we do not understand.";
+
+// 1. Print the number of characters in the text
+printOut("Number of characters: " + text.length);
+
+// 2. Print the character at position number 19
+printOut("Character at position 19: " + text.charAt(19));
+
+// 3. Print the characters starting at position number 35 and 8 characters forward
+printOut("Characters from position 35, 8 characters forward: " + text.substr(35, 8));
+
+// 4. Print the index at which 'earth' starts in the text
+printOut("Index of 'earth': " + text.indexOf("earth"));
+
+
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+/* Comparison, print the values for the following expressions (evaluate whether the statements are true):
+● Is 5 greater than 3?
+● Is 7 greater than or equal to 7?
+● Is "a" greater than "b"?
+● Is "1" less than "a"?
+● Is "2500" less than "abcd"?
+● "arne" is not equal to "thomas".
+● (2 equals 5) is this statement true?
+● ("abcd" is greater than "bcd") is this statement false?
+*/
+// Comparisons
+
+// 1. Is 5 greater than 3?
+printOut("Is 5 greater than 3? " + (5 > 3));
+
+// 2. Is 7 greater than or equal to 7?
+printOut("Is 7 greater than or equal to 7? " + (7 >= 7));
+
+// 3. Is "a" greater than "b"?
+printOut('Is "a" greater than "b"? ' + ("a" > "b"));
+
+// 4. Is "1" less than "a"?
+printOut('Is "1" less than "a"? ' + ("1" < "a"));
+
+// 5. Is "2500" less than "abcd"?
+printOut('Is "2500" less than "abcd"? ' + ("2500" < "abcd"));
+
+// 6. "arne" is not equal to "thomas".
+printOut('"arne" is not equal to "thomas"? ' + ("arne" !== "thomas"));
+
+// 7. (2 equals 5) is this statement true?
+printOut("(2 equals 5) is this statement true? " + (2 === 5));
+
+// 8. ("abcd" is greater than "bcd") is this statement false?
+printOut('("abcd" is greater than "bcd") is this statement false? ' + !("abcd" > "bcd"));
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+/* Convert and print the following expressions:
+● from text "254" to a number
+● from text "57.23" to a number
+● from text "25 kroner" to a number
+*/
+
+let number1 = parseInt("254");
+printOut(' "254" to number: ' + number1);
+
+let number2 = parseFloat("57.23");
+printOut(' "57.23" to number: ' + number2);
+
+let number3 = parseInt("25 kroner"); 
+printOut(' "25 kroner" to number: ' + number3);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+/* Create a variable "r" and randomly generate a number from 1 to 360 (1 >= r <= 360).
+*/
+
+const r = Math.floor(Math.random() * 360) + 1;
+
+printOut("Random number (r) between 1 and 360: " + r);
+
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+/*Use modulus (%) to calculate how many weeks and days are in 131 days.
+*/
+const totalDays = 131;
+
+// Calculate number of weeks and remaining days
+const weeks = Math.floor(totalDays / 7); 
+const Days = totalDays % 7;              
+
+printOut("Weeks: " + weeks + ", Days: " + Days);
+
 
 
